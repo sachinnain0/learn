c03cdfc2cf961eabc1e1df98d1d3fbf716cfafc4
Sachin Kumar <sachinnain0@gmail.com>
2018-06-02 14:08:39 +0530

    JSON validator completed

--- src/main/java/JSON_Validation/JSON_Validation/App.java
+++ src/main/java/JSON_Validation/JSON_Validation/App.java
@@ -1,13 +1,37 @@
 package JSON_Validation.JSON_Validation;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.everit.json.schema.Schema;
+import org.everit.json.schema.loader.SchemaLoader;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static void main( String[] args )
+	{
+		try {
+			InputStreamReader inputStream = new InputStreamReader(new FileInputStream("/home/sachin/eclipse-workspace/JSON-Validation/schema.json"));
+			InputStreamReader fileStream = new InputStreamReader(new FileInputStream("/home/sachin/eclipse-workspace/JSON-Validation/file.json"));
+			JSONObject rawSchema = new JSONObject(new JSONTokener(inputStream));
+			Schema schema = SchemaLoader.load(rawSchema);
+			
+			JSONObject rawfile = new JSONObject(new JSONTokener(fileStream));
+			
+			schema.validate(rawfile); // throws a ValidationException if this object is invalid
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+
+	}
 }
