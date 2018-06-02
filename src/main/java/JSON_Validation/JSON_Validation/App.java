8143935ae17729ce2d08e3561fcc12237dae91f4
Sachin Kumar <sachinnain0@gmail.com>
2018-06-02 12:03:31 +0530

    First Commit

---
+++ src/main/java/JSON_Validation/JSON_Validation/App.java
@@ -0,0 +1,13 @@
+package JSON_Validation.JSON_Validation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
